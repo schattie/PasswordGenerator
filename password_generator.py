c15dfe2a63d1160741e4a24aa2238475041c2aca
Hunter Schattie <hs49@students.uwf.edu>
2022-05-12 18:27:56 -0500

    passed all first test cases

--- password_generator.py
+++ password_generator.py
@@ -3,21 +3,28 @@ import string
 
 class PasswordGenerator:
     password = ""
-    passwordSize = 50
-    specialCharacters = False
-    numbers = False
+    password_size = 15
+    special_characters_choice = False
+    number_choice = False
     letters = string.ascii_letters
     numbers = string.digits
     symbols = string.punctuation
 
     def generate_password(self):
-        if(self.numbers and self.specialCharacters):
-            self.password = ''.join(random.choice(self.letters + self.numbers + self.symbols) for i in range(self.passwordSize))
-        elif(self.numbers):
-            self.password = ''.join(random.choice(self.letters + self.numbers) for i in range(self.passwordSize))
-        elif(self.specialCharacters):
-            self.password = ''.join(random.choice(self.letters + self.symbols) for i in range(self.passwordSize))
+        if(self.number_choice and self.special_characters_choice):
+            self.password = ''.join(random.choice(self.letters + self.numbers + self.symbols) for i in range(self.password_size))
+        elif(self.number_choice):
+            self.password = ''.join(random.choice(self.letters + self.numbers) for i in range(self.password_size))
+        elif(self.special_characters_choice):
+            self.password = ''.join(random.choice(self.letters + self.symbols) for i in range(self.password_size))
         else:
-            self.password = ''.join(random.choice(self.letters) for i in range(self.passwordSize))
-    def get_Password(self):
+            self.password = ''.join(random.choice(self.letters) for i in range(self.password_size))
+    
+    def get_password(self):
         return self.password
+
+    def set_special_characters(self, specialCharacters):
+        self.special_characters_choice = specialCharacters
+
+    def set_numbers(self, numbers):
+        self.number_choice = numbers
--- test_password_generator.py
+++ test_password_generator.py
@@ -2,55 +2,58 @@ import unittest
 import re
 from password_generator import PasswordGenerator
 
-passwordGenerator = PasswordGenerator()
-
 class PasswordGeneratorTest(unittest.TestCase):
+    passwordGenerator = PasswordGenerator()
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
     def test_specialcharacter_number(self):
-        passwordGenerator.generate_password()
+        self.passwordGenerator.set_numbers(True)
+        self.passwordGenerator.set_special_characters(True)
+        self.passwordGenerator.generate_password()
         number_result = False
         special_character_result = False
-        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
 
-        for character in passwordGenerator.get_Password():
+        for character in self.passwordGenerator.get_password():
             if character.isdigit():
                 number_result = True
                 break
 
-        if(regex.search(passwordGenerator.get_Password())):
+        if(self.regex.search(self.passwordGenerator.get_password())):
             special_character_result = True
 
         self.assertTrue(number_result)
         self.assertTrue(special_character_result)
 
     def test_number(self):
-        passwordGenerator.generate_password()
+        self.passwordGenerator.set_numbers(True)
+        self.passwordGenerator.set_special_characters(False)
+        self.passwordGenerator.generate_password()
         number_result = False
         special_character_result = False
-        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
 
-        for character in passwordGenerator.get_Password():
+        for character in self.passwordGenerator.get_password():
             if character.isdigit():
                 number_result = True
                 break
 
-        if(regex.search(passwordGenerator.get_Password())):
+        if(self.regex.search(self.passwordGenerator.get_password())):
             special_character_result = True
 
         self.assertTrue(number_result)
         self.assertFalse(special_character_result)
         
     def test_specialcharacter(self):
-        passwordGenerator.generate_password()
+        self.passwordGenerator.set_numbers(False)
+        self.passwordGenerator.set_special_characters(True)
+        self.passwordGenerator.generate_password()
         number_result = False
         special_character_result = False
-        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
 
-        for character in passwordGenerator.get_Password():
+        for character in self.passwordGenerator.get_password():
             if character.isdigit():
                 number_result = True
                 break
 
-        if(regex.search(passwordGenerator.get_Password())):
+        if(self.regex.search(self.passwordGenerator.get_password())):
             special_character_result = True
 
         self.assertFalse(number_result)
