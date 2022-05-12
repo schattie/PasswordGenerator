bfcd801417b7ad87ac65b20c0399dc71a8665076
Hunter Schattie <hs49@students.uwf.edu>
2022-05-11 21:08:04 -0500

    Created class structure

---
+++ password_generator.py
@@ -0,0 +1,24 @@
+import numbers
+from typing_extensions import Self
+
+
+class PasswordGenerator:
+    password = ""
+    passwordSize = 6
+    specialCharacters = False
+    numbers = False
+
+    def generate_password():
+        if(Self.numbers and Self.specialCharacters):
+            #generate password with letters numbers and characters
+            d
+        elif(Self.numbers):
+            #generate password with numbers and letters
+            d
+        elif(Self.specialCharacters):
+            #generate password with special characters and letter
+            d
+        else:
+            #generate password with only letters
+            d
+
