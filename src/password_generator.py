1744eba3615e3ecaf72881d66b7ec3fdc9b499d3
Hunter Schattie <hs49@students.uwf.edu>
2022-05-12 19:02:34 -0500

    added src file

---
+++ src/password_generator.py
@@ -0,0 +1,33 @@
+import random
+import string
+
+class PasswordGenerator:
+    password = ""
+    password_size = 15
+    special_characters_choice = False
+    number_choice = False
+    letters = string.ascii_letters
+    numbers = string.digits
+    symbols = string.punctuation
+
+    def generate_password(self):
+        if(self.number_choice and self.special_characters_choice):
+            self.password = ''.join(random.choices(self.letters + self.numbers + self.symbols, k=self.password_size))
+        elif(self.number_choice):
+            self.password = ''.join(random.choices(self.letters + self.numbers, k=self.password_size))
+        elif(self.special_characters_choice):
+            self.password = ''.join(random.choices(self.letters + self.symbols, k=self.password_size))
+        else:
+            self.password = ''.join(random.choices(self.letters, k=self.password_size))
+    
+    def get_password(self):
+        return self.password
+
+    def set_special_characters(self, specialCharacters):
+        self.special_characters_choice = specialCharacters
+
+    def set_numbers(self, numbers):
+        self.number_choice = numbers
+
+    def set_password_size(self, password_size):
+        self.password_size = password_size
